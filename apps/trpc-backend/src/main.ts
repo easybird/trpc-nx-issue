903a55f26fd1eed31136932d13c3b198b7dec8d7
easybird <easybird85@gmail.com>
2024-04-12 08:47:28 +0200

    init nx repo - add trpc - add generatePackageJson option

---
+++ apps/trpc-backend/src/main.ts
@@ -0,0 +1,46 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import express from 'express';
+import * as path from 'path';
+import { initTRPC } from '@trpc/server';
+
+/**
+ * Initialization of tRPC backend
+ * Should be done only once per backend!
+ */
+const t = initTRPC.create();
+
+/**
+ * Export reusable router and procedure helpers
+ * that can be used throughout the router
+ */
+export const router = t.router;
+export const publicProcedure = t.procedure;
+
+const appRouter = router({
+  userList: publicProcedure.query(async () => {
+    console.log('just a test');
+    return 'Hello World!';
+  }),
+});
+
+// Export type router type signature,
+// NOT the router itself.
+export type AppRouter = typeof appRouter;
+
+const app = express();
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/api', (req, res) => {
+  res.send({ message: 'Welcome to trpc-backend!' });
+});
+
+const port = process.env.PORT || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
